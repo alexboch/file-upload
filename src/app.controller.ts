fd39a4053c320a9ce025fd87298eb0515b521621
Alexander Bocharov <alexbocharov2@gmail.com>
2019-12-04 23:38:17 +0300

    uploading done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,50 @@
-import { Controller, Get, Post, UseInterceptors, UploadedFile, Response} from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile, Response } from '@nestjs/common';
 import { AppService } from './app.service';
-import {FileInterceptor} from '@nestjs/platform-express'
+import { FileInterceptor } from '@nestjs/platform-express'
 import * as express from 'express'
+import AWS = require('aws-sdk');
+const ID = 'AKIAIUHWQI25BSNZR52Q';
+const SECRET = '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';
+const BUCKET_NAME = 'fuckshit';
 
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
- 
+  constructor(private readonly appService: AppService) {
+
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+
+
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file, @Response() res:express.Response){
+  uploadFile(@UploadedFile() file, @Response() res: express.Response) {
     console.log(file);
-    res.sendFile('index.html');
+    let s = file.originalname;
+    // Setting up S3 upload parameters
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: file.originalname, // File name you want to save as in S3
+      Body: file.buffer
+    };
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err, data);
+        
+      }
+      else{
+        res.send("file successfully uploaded")
+      }
+    });
   }
 
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Aws from 'aws-sdk'
+
+
 
 @Module({
   imports: [],
